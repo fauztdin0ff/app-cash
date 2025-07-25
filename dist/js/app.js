caa524080ab5c6f50222adfa1f4088883296a824
Fauztdinov Damir <fauztdinov@gmail.com>
2025-07-25 15:03:38 +0500

    full-layout

--- dist/js/app.js
+++ dist/js/app.js
@@ -234,12 +234,12 @@ const timelines = [
          if (el) el.textContent = '0';
       })
       .add(animateCardHeader('.card-balance'))
-      .to('.card-balance-cursor', { opacity: 1, y: 132, duration: 1, ease: 'power2.out' })
-      .to('.card-balance-cursor', { y: 91, duration: 0.5 })
+      .to('.card-balance-cursor', { opacity: 1, y: 132, duration: 0.7, ease: 'power2.out' })
+      .to('.card-balance-cursor', { y: 91, duration: 0.4 })
       .to('.card-balance-cursor', { scale: 0.8, duration: 0.2 })
-      .to('.card-balance-cursor', { scale: 1, duration: 0.3 })
+      .to('.card-balance-cursor', { scale: 1, duration: 0.2 })
       .to('.card-balance__tel-sel', { background: 'rgba(168, 85, 247, 0.6)', duration: 0.1 }, "<")
-      .to('.card-balance__screen-1', { opacity: 0, y: "-50%", duration: 1 })
+      .to('.card-balance__screen-1', { opacity: 0, y: "-50%", duration: 0.4 })
       .to('.card-balance__screen-2', { opacity: 1, y: "0", duration: 1 })
       .to('.card-balance-cursor', { y: 58, x: -20, duration: 0.3 }, "-=0.3")
       .to('.card-balance-cursor', { scale: 0.8, duration: 0.2 })
@@ -276,7 +276,7 @@ const timelines = [
       .add(animateCardHeader('.card-boost'))
       .to('.card-boost__image svg', {
          clipPath: 'inset(0 0% 0 0)',
-         duration: 5,
+         duration: 3,
          ease: 'power2.out'
       })
       .add(() => {
@@ -285,7 +285,7 @@ const timelines = [
 
          gsap.to(obj, {
             val: 200000,
-            duration: 4,
+            duration: 3,
             ease: "power2.out",
             onUpdate: () => {
                numberElement.textContent = formatNumber(obj.val);
@@ -293,7 +293,7 @@ const timelines = [
          });
       }, "<")
       .add(() => animatePluses('.card-boost__graphic'), "<+0.3")
-      .to({}, { duration: 0.5 }),
+      .to({}, { duration: 0.8 }),
 
 
    //3 card
@@ -355,7 +355,7 @@ function animateCardHeader(cardSelector) {
             clipPath: 'inset(0 0% 0 0)',
             opacity: 1,
             delay: 1,
-            duration: 2,
+            duration: 1,
             ease: 'power2.out'
          },
          "-=0.3"
@@ -444,6 +444,211 @@ window.addEventListener("scroll", function () {
    }
 });
 
+
+/*------------------------------Service anim---------------------------*/
+document.addEventListener("DOMContentLoaded", () => {
+   const section = document.querySelector(".service");
+   const bgLine = document.querySelector(".service__map-road-bg");
+
+   const setBackgroundColorClass = (progress) => {
+      section.classList.remove("-bg-purple", "-bg-yellow", "-bg-green");
+
+      if (progress >= 0.75) {
+         section.classList.add("-bg-green");
+      } else if (progress >= 0.5) {
+         section.classList.add("-bg-yellow");
+      } else if (progress >= 0.25) {
+         section.classList.add("-bg-purple");
+      }
+   };
+
+   const updateProgress = () => {
+      const rect = section.getBoundingClientRect();
+      const windowHeight = window.innerHeight;
+      const sectionHeight = rect.height;
+      const viewportCenter = windowHeight / 2;
+      const distanceToCenter = viewportCenter - rect.top;
+      const progress = Math.min(Math.max(distanceToCenter / sectionHeight, 0), 1);
+
+      if (bgLine) {
+         bgLine.style.height = `${progress * 100}%`;
+      }
+
+      setBackgroundColorClass(progress);
+   };
+
+   window.addEventListener("scroll", updateProgress);
+   window.addEventListener("resize", updateProgress);
+   updateProgress();
+});
+
+
+/*------------------------------
+Faq
+---------------------------*/
+document.addEventListener("DOMContentLoaded", () => {
+   const faqItems = document.querySelectorAll(".faq__item");
+
+   if (faqItems.length === 0) return;
+
+   faqItems.forEach((item) => {
+      const question = item.querySelector(".faq__question");
+      const icon = item.querySelector(".faq__item-icon");
+      const answer = item.querySelector(".faq__answer");
+
+      if (!question || !icon || !answer) return;
+
+      const toggleFaqItem = () => {
+         const isActive = item.classList.contains("active");
+
+         faqItems.forEach((el) => {
+            const elAnswer = el.querySelector(".faq__answer");
+            if (elAnswer) {
+               el.classList.remove("active");
+               elAnswer.style.maxHeight = null;
+            }
+         });
+
+         if (!isActive) {
+            item.classList.add("active");
+            answer.style.maxHeight = answer.scrollHeight + 40 + "px";
+         }
+      };
+
+      question.addEventListener("click", toggleFaqItem);
+      icon.addEventListener("click", toggleFaqItem);
+   });
+});
+
+
+/*------------------------------Слайдер галерея---------------------------*/
+document.addEventListener('DOMContentLoaded', function () {
+   const gallerySliderElement = document.querySelector('.testimonials__slider');
+   let gallerySlider;
+
+   function initSlider() {
+      if (!gallerySlider) {
+         gallerySlider = new Swiper(gallerySliderElement, {
+            loop: true,
+            freeMode: false,
+            slidesPerView: 'auto',
+            watchOverflow: true,
+            grabCursor: true,
+            breakpoints: {
+               320: {
+                  spaceBetween: 10,
+               },
+               767: {
+                  spaceBetween: 19,
+               },
+               1000: {
+                  spaceBetween: 30,
+               },
+               1920: {
+                  spaceBetween: 38,
+               },
+            },
+         });
+      }
+   }
+
+   initSlider();
+});
+
+const screenGroups = document.querySelectorAll(".testimonials__slide-screens");
+
+screenGroups.forEach(group => {
+   const imgs = group.querySelectorAll("img");
+
+   imgs.forEach((img, index) => {
+      img.addEventListener("click", (e) => {
+         e.stopPropagation();
+
+         const dynamicEl = [...imgs].map(el => ({
+            src: el.src,
+            thumb: el.src,
+            subHtml: '',
+         }));
+
+         // создаём временный элемент для инициализации
+         const tempContainer = document.createElement("div");
+         document.body.appendChild(tempContainer);
+
+         const popup = lightGallery(tempContainer, {
+            dynamic: true,
+            download: false,
+            dynamicEl,
+            mobileSettings: {
+               showCloseIcon: true,
+            },
+         });
+
+         popup.openGallery(index);
+
+         // Удаляем контейнер после закрытия галереи
+         popup.on('lgAfterClose', () => {
+            popup.destroy();
+            tempContainer.remove();
+         });
+      });
+   });
+});
+
+/*------------------------------Calc range---------------------------*/
+const sliderContainer = document.querySelector('.slider-container');
+const minValue = parseInt(sliderContainer.getAttribute('data-min'));
+const maxValue = parseInt(sliderContainer.getAttribute('data-max'));
+const step = 100;
+const slider = document.querySelector('.range-slider');
+const thumb = document.querySelector('.range-slider__thumb');
+const tooltip = document.querySelector('.range-slider__tooltip');
+const resultValue = document.querySelector('.calculator__result-value');
+let isDragging = false;
+
+function updateSlider(position) {
+   const sliderRect = slider.getBoundingClientRect();
+   let percentage = (position - sliderRect.left) / sliderRect.width;
+   percentage = Math.min(Math.max(percentage, 0), 1);
+   let value = minValue + percentage * (maxValue - minValue);
+   value = Math.round(value / step) * step;
+   value = Math.min(Math.max(value, minValue), maxValue);
+   thumb.style.left = `${percentage * 100}%`;
+   let tooltipLeft = percentage * 100;
+   tooltipLeft = Math.min(tooltipLeft, 80);
+   tooltip.style.left = `${tooltipLeft}%`;
+   tooltip.innerText = `${value.toLocaleString()} руб`;
+
+   // 💰 Выигрыш = (ставка * 6) * 0.7
+   const winnings = Math.round((value * 9 - value) * 0.7);
+   resultValue.innerText = `${winnings.toLocaleString()} руб.`;
+}
+
+
+function onMove(e) {
+   if (isDragging) {
+      const clientX = e.touches ? e.touches[0].clientX : e.clientX;
+      updateSlider(clientX);
+   }
+}
+
+function startDragging(e) {
+   isDragging = true;
+   document.body.style.cursor = 'grabbing';
+   e.preventDefault();
+}
+
+function stopDragging() {
+   isDragging = false;
+   document.body.style.cursor = 'default';
+}
+
+thumb.addEventListener('mousedown', startDragging);
+document.addEventListener('mousemove', onMove);
+document.addEventListener('mouseup', stopDragging);
+thumb.addEventListener('touchstart', startDragging);
+document.addEventListener('touchmove', onMove);
+document.addEventListener('touchend', stopDragging);
+updateSlider(slider.getBoundingClientRect().left + (30000 - minValue) / (maxValue - minValue) * slider.clientWidth);
 })();
 
 /******/ })()
